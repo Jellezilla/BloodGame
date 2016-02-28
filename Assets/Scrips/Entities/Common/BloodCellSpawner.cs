dca950029f440af045f1ac6dba12a5ad826bd8fd
Jellezilla <jesper@tjorne.dk>
2016-02-28 17:39:11 +0100

    BloodCell Spawning done

--- Assets/Scrips/Entities/Common/BloodCellSpawner.cs
+++ Assets/Scrips/Entities/Common/BloodCellSpawner.cs
@@ -3,13 +3,56 @@ using System.Collections;
 
 public class BloodCellSpawner : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    private GameObject bloodCellPrefab;
+    private float spawnTime = 3f;
+
+    private GameObject[] spawnPoints;
+
+    // Use this for initialization
+    void Start () {
+        bloodCellPrefab = Resources.Load("BloodCell") as GameObject;
+
+        
+        spawnPoints = GetSpawnPoints();
+
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+    /// <summary>
+    /// Returns all flowareas in level tagged as "FlowArea"
+    /// </summary>
+    /// <returns></returns>
+   GameObject[] GetSpawnPoints()
+    {
+        GameObject[] tmp;
+        tmp = GameObject.FindGameObjectsWithTag("FlowArea");
+        return tmp;
+    }
+
+    /// <summary>
+    /// Spawns a bloodcell every <spawnTime> second at a random location within a random picked flowArea.
+    /// </summary>
+    void Spawn()
+    {
+
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+
+        float offsetX = 0.0f;
+        float offsetZ = 0.0f;
+
+        BoxCollider bc = spawnPoints[spawnPointIndex].GetComponent<BoxCollider>();
+        offsetX = Random.Range(-(bc.size.x / 2.2f), (bc.size.x / 2.2f));
+        offsetZ = Random.Range(-(bc.size.z / 2.2f), (bc.size.z / 2.2f));
+
+        Vector3 spawnPos = new Vector3(spawnPoints[spawnPointIndex].transform.position.x + offsetX, spawnPoints[spawnPointIndex].transform.position.y, spawnPoints[spawnPointIndex].transform.position.z + offsetZ);
+        
+        GameObject clone = Instantiate(bloodCellPrefab, spawnPos, Quaternion.identity) as GameObject;
+
+        string output = "Spawned: " + bloodCellPrefab.name;
+        output += ". At pos: " + spawnPos.ToString();
+        output += ". At spawnPoint: " + spawnPoints[spawnPointIndex].name;
+        Debug.Log(output);
+
+        clone.transform.parent = transform;
+    }
+
 }
