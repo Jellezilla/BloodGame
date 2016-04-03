76b7ab45803528d87f32673dda983c4f6ddb1a64
Alex Busnita <alexandru.busnita@yahoo.ro>
2016-04-03 07:33:23 +0200

    ++ Added new assets  : ++Vehicle Controller ++ Jeep -- Moved Old Assets : Assets must be migrated first

---
+++ Assets/Resources/Vehicle/Scripts/VehicleTurret.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(VehicleUserControl))]
+public class VehicleTurret : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject m_VehicleTurret;
+    [SerializeField]
+    private float spawnPointOffset;
+    private Vector3 m_spawnPoint;
+    private float m_OldRotation;
+    [Range(0,1)]
+    [SerializeField]
+    private float m_rotationAsist;
+	// Use this for initialization
+	void Start () {
+
+
+
+    }
+
+
+    private void CalculateSpawnPoint()
+    {
+
+    }
+
+    void Update()
+    {
+        Aim();
+    }
+    public void Aim()
+    {
+        m_VehicleTurret.transform.LookAt(new Vector3(Input.mousePosition.x,0, Input.mousePosition.z));
+    }
+
+    public void Fire()
+    {
+
+    }
+	
+}
---
+++ Assets/Resources/Vehicle/Scripts/VehicleUserControl.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(VehicleController))]
+public class VehicleUserControl : MonoBehaviour {
+
+    private VehicleController m_controller;
+    private float m_hAxis;
+    private float m_vAxis;
+    private float m_brakeAxis;
+	// Use this for initialization
+	void Awake ()
+    {
+        m_controller = GetComponent<VehicleController>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        m_hAxis = Input.GetAxis("Horizontal");
+        m_vAxis = Input.GetAxis("Vertical");
+        m_brakeAxis = Input.GetAxis("Jump");
+	
+	}
+
+    void FixedUpdate()
+    {
+        m_controller.Motion(m_vAxis, m_hAxis, m_vAxis, m_brakeAxis);
+        m_brakeAxis = 0;
+    }
+}
